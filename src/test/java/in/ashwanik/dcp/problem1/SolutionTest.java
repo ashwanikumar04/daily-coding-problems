13e1c138393516c9729d150b98f57188057d9535
Ashwani Kumar <ashwanikumar04@gmail.com>
2019-04-23 17:20:47 +0100

    Added base structure

---
+++ src/test/java/in/ashwanik/dcp/problem1/SolutionTest.java
@@ -0,0 +1,19 @@
+package in.ashwanik.dcp.problem1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class SolutionTest {
+    @Test
+    void testSumExistsForNull() {
+        int[] array = null;
+        assertFalse(new Solution().sumExists(array, 5));
+    }
+
+    @Test
+    void testSumExistsTrue() {
+        int[] array = {10, 5};
+        assertFalse(new Solution().sumExists(array, 10));
+    }
+}
---
+++ src/test/java/in/ashwanik/dcp/problem2/SolutionTest.java
@@ -0,0 +1,16 @@
+package in.ashwanik.dcp.problem2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    @Test
+    void testProduct() {
+        int[] array = {3, 2, 1};
+        int[] result = new Solution().product(array);
+        assertEquals(2, result[0]);
+        assertEquals(3, result[1]);
+        assertEquals(6, result[2]);
+    }
+}
