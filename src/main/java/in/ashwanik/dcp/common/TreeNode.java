3e6265751c7ff437e6752a205f7a685cf3c03237
Ashwani Kumar <ashwanikumar04@gmail.com>
2019-04-24 15:04:19 +0100

    Added problem #3

---
+++ src/main/java/in/ashwanik/dcp/common/TreeNode.java
@@ -0,0 +1,79 @@
+package in.ashwanik.dcp.common;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ */
+public class TreeNode<T> {
+
+    private T data;
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+
+    public TreeNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public TreeNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode<T> right) {
+        this.right = right;
+    }
+
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void printTree() {
+        Queue<TreeNode<T>> queue = new LinkedList<>();
+
+        queue.add(this);
+        queue.add(null);
+        while (!queue.isEmpty()) {
+            TreeNode<T> current = queue.remove();
+            if (current == null) {
+                if (!queue.isEmpty()) {
+                    queue.add(null);
+                    System.out.print("\n");
+                }
+            } else {
+                System.out.print(current.data + "\t");
+                if (current.getLeft() != null) {
+                    queue.add(current.getLeft());
+                }
+                if (current.getRight() != null) {
+                    queue.add(current.getRight());
+                }
+            }
+
+        }
+        System.out.print("\n");
+        System.out.print("\n");
+    }
+
+    @Override
+    public String toString() {
+        return data + "";
+    }
+}
+
+
+
+
---
+++ src/main/java/in/ashwanik/dcp/problem3/Solution.java
@@ -0,0 +1,82 @@
+package in.ashwanik.dcp.problem3;
+
+import in.ashwanik.dcp.common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Solution {
+
+    String serialize(TreeNode<String> root) {
+        if (root == null) {
+            return "";
+        }
+        List<String> nodes = new ArrayList<>();
+        Deque<TreeNode<String>> queue = new ArrayDeque<>();
+        queue.push(root);
+
+        ///Adding dummy node as Deque does not allow null
+        TreeNode<String> dummy = new TreeNode<>("");
+
+        while (!queue.isEmpty()) {
+            TreeNode<String> node = queue.poll();
+            if (node != dummy) {
+                nodes.add(node.getData());
+                if (node.getLeft() == null) {
+                    queue.offer(dummy);
+                } else {
+                    queue.offer(node.getLeft());
+                }
+                if (node.getRight() == null) {
+                    queue.offer(dummy);
+                } else {
+                    queue.offer(node.getRight());
+                }
+            } else {
+                nodes.add("#");
+            }
+        }
+        return String.join(",", nodes);
+
+    }
+
+    TreeNode<String> deserialize(String data) {
+        if (data == null || data.isEmpty() || data.startsWith("#")) {
+            return null;
+        }
+        String[] nodes = data.split(",");
+        if (nodes.length == 0) {
+            return null;
+        }
+
+        TreeNode<String> root = new TreeNode<>("0");
+        Deque<TreeNode<String>> queue = new ArrayDeque<>();
+        queue.add(root);
+        TreeNode<String> node;
+        int index = 0;
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            if (!node.getData().equals("#")) {
+                node.setData(nodes[Integer.parseInt(node.getData())]);
+                int left = index + 1;
+                int right = index + 2;
+
+                if (left < nodes.length - 1 && !nodes[left].equals("#")) {
+                    TreeNode<String> leftNode = new TreeNode<>(Integer.toString(left));
+                    node.setLeft(leftNode);
+                    queue.offer(leftNode);
+                }
+                if (right < nodes.length - 1 && !nodes[right].equals("#")) {
+                    TreeNode<String> rightNode = new TreeNode<>(Integer.toString(right));
+                    node.setRight(rightNode);
+                    queue.offer(rightNode);
+                }
+            }
+            index += 2;
+        }
+
+        return root;
+    }
+}
