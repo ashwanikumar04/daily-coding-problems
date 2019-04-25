f1c6392d32fbf12b27e85c123785474d463f6e07
Ashwani Kumar <ashwanikumar04@gmail.com>
2019-04-25 14:56:13 +0100

    added p4

---
+++ src/main/java/in/ashwanik/dcp/problems/p4/Solution.java
@@ -0,0 +1,45 @@
+package in.ashwanik.dcp.problems.p4;
+
+public class Solution {
+
+    public int missingInteger(int[] array) {
+        if (array == null || array.length == 0) {
+            return 1;
+        }
+        int size = array.length;
+        int shift = partitionArrayInPositiveAndNegative(array);
+
+        int newSize = size - shift;
+
+        for (int current = shift; current < array.length; current++) {
+            int abs = Math.abs(array[current]);
+            if (abs - 1 + shift < newSize + shift && array[abs - 1 + shift] > 0) {
+                array[abs - 1 + shift] = -array[abs - 1 + shift];
+            }
+        }
+
+        for (int current = shift; current < array.length; current++) {
+            if (array[current] > 0) {
+                return current - shift + 1;
+            }
+        }
+        return newSize + 1;
+    }
+
+    private int partitionArrayInPositiveAndNegative(int[] array) {
+        int numberOfNegativeNumbers = 0;
+        int start = 0;
+
+
+        for (start = 0; start < array.length; start++) {
+
+            if (array[start] <= 0) {
+                int temp = array[start];
+                array[start] = array[numberOfNegativeNumbers];
+                array[numberOfNegativeNumbers] = temp;
+                numberOfNegativeNumbers++;
+            }
+        }
+        return numberOfNegativeNumbers;
+    }
+}
