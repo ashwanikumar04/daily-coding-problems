8f9b1b289b405ab4959f0d52f535decd77b95c4f
Ashwani Kumar <ashwanikumar04@gmail.com>
2019-05-08 12:16:48 +0100

    added p15

---
+++ src/main/java/in/ashwanik/dcp/problems/p15/Readme.md
@@ -0,0 +1,2 @@
+Given a stream of elements too large to store in memory, pick a random element
+from the stream with uniform probability.
---
+++ src/test/java/in/ashwanik/dcp/problems/p15/SolutionTest.java
@@ -0,0 +1,15 @@
+package in.ashwanik.dcp.problems.p15;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void testGetRandom() {
+
+
+        assertEquals(2, new Solution().getRandom(2));
+    }
+}
