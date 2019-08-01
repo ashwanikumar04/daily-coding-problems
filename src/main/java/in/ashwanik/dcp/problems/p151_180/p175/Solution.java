f2a90802328d3246a9109085c0f85cf3eaee1f66
Ashwani Kumar <ashwanikumar04@gmail.com>
2019-08-01 17:01:05 +0100

    p175

--- src/main/java/in/ashwanik/dcp/problems/p151_180/p175/Solution.java
+++ src/main/java/in/ashwanik/dcp/problems/p151_180/p175/Solution.java
@@ -2,46 +2,26 @@ package in.ashwanik.dcp.problems.p151_180.p175;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 class Solution {
-    ///https://www.datacamp.com/community/tutorials/markov-chains-python-tutorial
+    ///https://medium.com/@__amol__/markov-chains-with-python-1109663f3678
     Map<String, Integer> getMarkovChain(List<State> statesList, int numberOfIterations, String startState) {
         Map<String, Integer> result = new HashMap<>();
         if (statesList == null || statesList.isEmpty()) {
             return result;
         }
 
-        Set<String> statesSet = new LinkedHashSet<>();
+        Map<String, List<State>> stateMap = new HashMap<>();
 
         for (State state : statesList) {
-            statesSet.add(state.from);
+            stateMap.putIfAbsent(state.from, new ArrayList<>());
+            stateMap.get(state.from).add(state);
         }
-
-        List<String> states = new ArrayList<>(statesSet);
-
-        Map<Integer, List<Integer>> indicesMap = new HashMap<>();
-        int startIndex = 0;
-        for (int index = 0; index < states.size(); index++) {
-            List<Integer> list = new ArrayList<>();
-            int current = 0;
-            while (current < states.size()) {
-                list.add(startIndex++);
-                current++;
-            }
-            indicesMap.put(index, list);
-        }
-
         String currentState = startState;
-        float startProbability = 1;
-
         for (int iteration = 0; iteration < numberOfIterations; iteration++) {
-            int stateIndex = states.indexOf(currentState);
-            State state = getRandom(statesList, indicesMap.get(stateIndex));
-            startProbability = startProbability * state.prob;
+            State state = getRandom(stateMap.get(currentState));
             currentState = state.to;
             result.put(currentState, result.getOrDefault(currentState, 0) + 1);
         }
@@ -49,13 +29,13 @@ class Solution {
     }
 
 
-    private State getRandom(List<State> stateList, List<Integer> indices) {
+    private State getRandom(List<State> transitions) {
         double prob = Math.random();
         double cumulativeProb = 0.0;
-        for (int index : indices) {
-            cumulativeProb += stateList.get(index).prob;
+        for (State state : transitions) {
+            cumulativeProb += state.prob;
             if (prob <= cumulativeProb) {
-                return stateList.get(index);
+                return state;
             }
         }
         return null;
@@ -71,18 +51,6 @@ class Solution {
             this.from = from;
             this.to = to;
             this.prob = prob;
-        }
-
-        public String getFrom() {
-            return from;
-        }
-
-        public String getTo() {
-            return to;
-        }
-
-        public float getProb() {
-            return prob;
         }
     }
 }
